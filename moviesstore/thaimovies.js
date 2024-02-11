74cd43fbe99f2b75fd35be4b96da69b579269caf
xnxxlive <155718186+xnxxlive@users.noreply.github.com>
2024-02-12 03:47:52 +0630

    Update thaimovies.js

--- moviesstore/thaimovies.js
+++ moviesstore/thaimovies.js
@@ -1,5 +1,5 @@
 
-const url = "https://40f0cd47-5b08-4846-815a-154830637e85-00-3kfucwm1ywejb.sisko.replit.dev/moviesstore/thaimoviesm3u.html"
+const url = "https://xnxxlive.github.io/moviesstore/thaimoviesm3u.html"
 fetch(url)
   .then((response) => {
     const urlData = response.json();
