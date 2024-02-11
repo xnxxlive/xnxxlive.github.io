66f860175ce88e9528b6f1984ccafc4da8ed3551
xnxxlive <155718186+xnxxlive@users.noreply.github.com>
2024-02-12 03:59:10 +0630

    Update 2024movies.js

--- moviesstore/2024movies.js
+++ moviesstore/2024movies.js
@@ -1,5 +1,5 @@
 
-const url = "https://40f0cd47-5b08-4846-815a-154830637e85-00-3kfucwm1ywejb.sisko.replit.dev/moviesstore/2024moviesm3u.html"
+const url = "https://xnxxlive.github.io/moviesstore/2024moviesm3u.html"
 fetch(url)
   .then((response) => {
     const urlData = response.json();
