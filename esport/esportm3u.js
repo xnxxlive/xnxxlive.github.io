9bf058e309eab3f683ad13b2ca83e733eb5c0e49
xnxxlive <155718186+xnxxlive@users.noreply.github.com>
2024-02-12 01:03:12 +0630

    Add files via upload

---
+++ esport/esportm3u.js
@@ -0,0 +1,122 @@
+[
+{
+"url1": 
+"tv.mahar.live/esports/mahar.stream/playlist.m3u8",
+"url2": 
+"filex.tv:8080/@Ex0meen/azHypnxKKLy7/84957",
+"url3": 
+"cdn.klowdtv.net/803B48A/n1.klowdtv.net/live2/gsn_720p/playlist.m3u8",
+"url4": "45.71.252.16:8000/play/a0ho/index.m3u8",
+"url5": "video-weaver.sin01.hls.ttvnw.net/v1/playlist/CvoFl_c2hMOOqE76uKSGMuBou86x-Nf1EtmfKy_SJqNUBb6yCHOTF2cIJ23UJg0zSO42UVnpJ_GjkoWSVNtyYPUVVBRwhvxWee3ZZhElMSMuJvLAvUuwCJq2PVR7wN12MvbUr7Ws14z7FPOcP9muZetyEPVfe2vg7bIdMHCXgrhKICg7SytKE9nYfYx0lQwDneUo9tc3y8_OvAXHJ0rusqX7LqJesQoqW2QWw0-PkmFGA_XLdJCvcCNcXrt0Sp-QycAYUiqtY0bYHsuzfNXfTxTfEa4z5gIdaRLktN-90weAcb72CzkoPHxHrMf7KUfkqOOJZ78DvqA-_HSQ-0sHs1zwWm1tvMWb0nRNTuE_iCN2JoPeCQ_BRq0uY9FK3YJDi5x2fCRlH_DfBeQj-gnWAAiu6KAWtds0WN1EjYdssxXpMlJ0w_e6ruhFDCgyHm_luq8od0XmsBrgC1h29FzVYWXLq7JdwJvIUlhVecYm3u8751xrpSAWfXt3puPKJ9bVOF3GFx6gvA2XhXYa57pPLSB1oU_dyM0Ke2qb3PaLbaJNagyjepOWbPd23TtuCatCmtc1xWw-UxNwtlBQXLaWkv9JarjRwLXQbBh997G1OVitonSSHgcijY3ZBsY6RN-iqTl9uOBF5Fi6gAr9W1INgHfA_jhzeqhSIFZTuGuInyBYdG8OYSkekxCKa7NoO9gkv4DAz4vBMr15qwxURpFm4qxJnn7UN75iq5SxHygEEbPOhywKQkI1aiHPwk1mbY6CPoweCJWcNgqdFPH9uta7PK4VawaiOiEh65kF48-o8-vJYa5zFXT1GX2Fzarr0hqVRUSgMltBH0H-h0jAOobhx0tCkcoD11ulXsII3EBkSyjDoIrllBBldu-NbxPqOt9jUHuEf1ElZGS8penH-ad5u8LiHYLeV5AapmhGox49HVY2FFQPv1YIBVbWE7LfeNoef2ySbB-KircwDF5ajeGErTzl_NfKhe5GK4Tkgt-18OWdbeq5CmnxDgj95kpBGgxZMFz-eTtSlzoAiAUgASoJZXUtd2VzdC0yMPkH.m3u8"
+},
+
+{
+"url1": 
+"tstv-stream.tsm.utexas.edu/hls/livestream_hi/index.m3u8",
+"url2": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER2hd.m3u8",
+"url3": 
+"1123139281.cdn.khmer-hd.com/may1HD/chunks.m3u8",
+"url4": "threes.livetwoassia.online/hls/67.m3u8?md5=1kO-XgZhz637Dlt7ejPT7Q&expires=1698497986",
+"url5": "kuchini.site:8080/infosat/df3093b6a3/197711"
+},
+
+{
+"url1": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER3sd.m3u8",
+"url2": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER3hd.m3u8",
+"url3":
+"179720687.global.cdnfastest.com/live/sd-pxwrxlhx6v7wryk/chunklist_w1638830826.m3u8",
+"url4": "179720687.global.cdnfastest.com/live/sd-pxwrxlhx6v7wryk/chunklist_w1638830826.m3u8",
+"url5": "bl.uma.media/live/126865/HLS/6866944_4/2/1/playlist.m3u8"
+},
+
+{
+"url1": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER4sd.m3u8",
+"url2": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER4hd.m3u8",
+"url3": "obevcimanyd179314182.thapcam.link/live/mySD/chunklist.m3u8",
+"url4": "obevcimanyd179314182.thapcam.link/live/mySD/chunklist.m3u8",
+"url5": "kuchini.site:8080/infosat/df3093b6a3/12638"
+},
+
+{ "url1": "windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER5sd.m3u8",
+"url2": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER5hd.m3u8",
+"url3": "obevcimanyd179314182.thapcam.link/live/may2SD/chunklist.m3u8",
+"url4": "dai.google.com/linear/hls/event/r-eLp41YTHWTagvQSXFtAQ/master.m3u8", 
+"url5": "kuchini.site:8080/infosat/df3093b6a3/1483" },
+
+{ "url1": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER6sd.m3u8", 
+"url2": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER6hd.m3u8",
+"url3": 
+"1123139281.cdn.khmer-hd.com/soiHD/chunks.m3u8", 
+"url4": "dai.google.com/linear/hls/event/BCOFZq1JQjq12fmaO6lAAA/master.m3u8", 
+"url5": "bananatv.org:25461/live/bananatv/bananatv/349.ts" }, 
+
+{ "url1": "windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER7sd.m3u8",
+"url2": 
+"windowithiossmart-1.xx-multiple-tec.repl.co/SUPERPOWER7hd.m3u8", 
+"url3": "obevcimanyd179314182.thapcam.link/live/may2SD/chunklist.m3u8?|Referer=https://player.4shares.live/",
+"url4": "bl.uma.media/live/312119/HLS/4614144_3/2/1/playlist.m3u8", 
+"url5": "kuchini.site:8080/infosat/df3093b6a3/273" }, 
+
+{ "url1": "pull01.rrwes.live/live/stream-9911969_lsd.m3u8?txSecret=36acb0162d684ab61f1719217b830df6&txTime=653d6308", 
+"url2": 
+"pull01.rrwes.live/live/stream-9911969_lhd.m3u8?txSecret=098153bf333c4cb72bf393e150b2f477&txTime=653d6308", 
+"url3": "obevcimanyd179314182.thapcam.link/live/phoSD/chunklist.m3u8", 
+"url4": "dai.google.com/linear/hls/event/V9h-iyOxRiGp41ppQScDSQ/master.m3u8", 
+"url5": "kuchini.site:8080/infosat/df3093b6a3/279255" },
+
+{ "url1": "pull01.rrwes.live/live/stream-9912084_lsd.m3u8?txSecret=5d3427c6984856c46fc6e3bd9ad038e9&txTime=653d6290", 
+"url2": "pull01.rrwes.live/live/stream-9912084_lhd.m3u8?txSecret=87e8774f9db653efa108a759d42d3d55&txTime=653d6254", 
+"url3": "pull.niues.live/live/stream-491685_lhd.m3u8?auth_key=1698349484-0-0-d8f94fec3d46edfa9ba0ff055a20bada", 
+"url4": "dai.google.com/linear/hls/event/BCOFZq1JQjq12fmaO6lAAA/master.m3u8", 
+"url5": "bananatv.org:25461/live/bananatv1/lovebanana/349.ts" },
+
+{ "url1": "pull01.rrwes.live/live/stream-9912033_lsd.m3u8?txSecret=1d4073f8338f0c1c79bc0732b703dac4&txTime=653d62cc", "url2": "pull01.rrwes.live/live/stream-9912033_lhd.m3u8?txSecret=202e9d8f7c32d2da0d4c623d839003d2&txTime=653d62cc", "url3": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url4": "dai.google.com/linear/hls/event/BCOFZq1JQjq12fmaO6lAAA/master.m3u8", 
+"url5": "bananatv.org:25461/live/bananatv1/lovebanana/348.ts" }, 
+
+{ "url1": "pull01.rrwes.live/live/stream-359960_lsd.m3u8?txSecret=488215054101811bafa4d99683f20a0a&txTime=653d7eb0", "url2": "pull01.rrwes.live/live/stream-359960_lhd.m3u8?txSecret=ab3d50e26d6327e7f13ca32222d78fb1&txTime=653d7eb0", "url3": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url4": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/35.ts" }, 
+
+{ "url1": "pull.niues.live/live/stream-9912061_lsd.m3u8?auth_key=1698511544-0-0-e20d6925d5f0dc2cec41a83126fe3cc0", "url2": "pull.niues.live/live/stream-9912061_lhd.m3u8?auth_key=1698511544-0-0-066c734c07d0d0671de8bc0465cc3ac0", "url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/348.ts" }, 
+
+{ "url1": "pull01.rrwes.live/live/stream-9912009_lsd.m3u8?txSecret=d04e99cfc791cf4fa6f462176a5c20f1&txTime=653d8540", 
+"url2": "pull01.rrwes.live/live/stream-9912009_lhd.m3u8?txSecret=84ea7ff5aae7becc1bb262315f4ff92b&txTime=653d8540", 
+"url3": "1123139281.cdn.khmer-hd.com/may1HD/chunks.m3u8?|Referer=https://player.4shares.live/", 
+"url4": "vsd217.mycdn.me/hls/5644453939794.m3u8/sig/AxWqP41kG_E/expires/1698572547940/srcIp/206.189.145.118/urls/185.226.52.55/clientType/1/srcAg/UNKNOWN_ANDROID/mid/7270835035730/video.m3u8?p", 
+"url5": "bananatv.org:25461/live/bananatv1/lovebanana/117.m3u8" }, 
+
+
+{ "url1": "pull01.rrwes.live/live/stream-462413_lsd.m3u8?txSecret=f38a206b230547893995a0df0991d105&txTime=653d84c8", "url2": "pull01.rrwes.live/live/stream-462413_lhd.m3u8?txSecret=c08ae8c193e0fc1e897edf4a259c28df&txTime=653d84c8", "url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/35.ts" }, 
+
+{ "url1": "pull.niues.live/live/stream-9911977_lsd.m3u8?auth_key=1698512564-0-0-12066b9293f490f27a15a50b5b838549", "url2": "pull.niues.live/live/stream-9911977_lhd.m3u8?auth_key=1698512564-0-0-6fdd7ec305e8052c70524c77a20d6d6a", "url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", "url5": "kuchini.site:8080/infosat/df3093b6a3/271" }, 
+
+
+{ "url1": "pull01.rrwes.live/live/stream-9912019_lsd.m3u8?txSecret=bca71c8f1548d1e6beb2b9251665f654&txTime=653da82c", 
+"url2": "pull01.rrwes.live/live/stream-9912019_lhd.m3u8?txSecret=bc8688964173951acb88e508c5e4d940&txTime=653da82c", 
+"url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", 
+"url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", 
+"url5": "kuchini.site:8080/infosat/df3093b6a3/298471" }, 
+
+{ "url1": "pull.niues.live/live/stream-9912077_lsd.m3u8?auth_key=1698521504-0-0-3085cc4714c0526a53455da87be77abf", 
+"url2": "pull.niues.live/live/stream-9912077_lhd.m3u8?auth_key=1698521504-0-0-9bfa0031c2be8b994aae19bb7c802869", 
+"url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", 
+"url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", 
+"url5": "bananatv.org:25461/live/bananatv1/lovebanana/349.ts" },
+
+{ "url1": "pull.niues.live/live/stream-359960_lsd.m3u8?auth_key=1698521564-0-0-42b039a4ad2882716621b5c823d6901e", "url2": "pull.niues.live/live/stream-359960_lhd.m3u8?auth_key=1698521564-0-0-05b47929ea25210b36a8faa48dc7422a", "url3": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url4": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/348.ts" }, 
+
+{ "url1": "pull.niues.live/live/stream-9912050_lsd.m3u8?auth_key=1698156824-0-0-5eb4d7890240cba5db690a0533df3398", "url2": "pull.niues.live/live/stream-9912050_lhd.m3u8?auth_key=1698156824-0-0-707f8e308696c74c362dfedc876847fb", "url3": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url4": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/35.ts" }, 
+
+{ "url1": "pull.niues.live/live/stream-9912075_lsd.m3u8?auth_key=1698156584-0-0-3815f04c18d314671d603ed0490b52c4", "url2": "pull.niues.live/live/stream-9912075_lhd.m3u8?auth_key=1698156524-0-0-a9fbfa91f7e6aee21b6a70ca5762d906", "url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", 
+"url5": "bananatv.org:25461/live/bananatv1/lovebanana/348.ts" }, 
+
+{ "url1": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/2144853", "url2": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", "url5": "kuchini.site:8080/infosat/df3093b6a3/279255" },
+{ "url1": "pull.niues.live/live/stream-9912075_lsd.m3u8?auth_key=1698156584-0-0-3815f04c18d314671d603ed0490b52c4", "url2": "pull.niues.live/live/stream-9912075_lhd.m3u8?auth_key=1698156524-0-0-a9fbfa91f7e6aee21b6a70ca5762d906", "url3": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url4": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/348.ts" }, { "url1": "pull.niues.live/live/stream-9912050_lsd.m3u8?auth_key=1698156824-0-0-5eb4d7890240cba5db690a0533df3398", "url2": "pull.niues.live/live/stream-9912050_lhd.m3u8?auth_key=1698156824-0-0-707f8e308696c74c362dfedc876847fb", "url3": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url4": "pull.niues.live/live/stream-278654_lhd.m3u8?auth_key=1698132344-0-0-5fd6085095885f7d51122c15473b3408", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/35.ts" }, { "url1": "pull.niues.live/live/stream-9912075_lsd.m3u8?auth_key=1698156584-0-0-3815f04c18d314671d603ed0490b52c4", "url2": "pull.niues.live/live/stream-9912075_lhd.m3u8?auth_key=1698156524-0-0-a9fbfa91f7e6aee21b6a70ca5762d906", "url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", "url5": "bananatv.org:25461/live/bananatv1/lovebanana/348.ts" }, { "url1": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/2144853", "url2": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url3": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219090", "url4": "sdmx.vip:8080/restreamiptv/uHbQMnZaPxaQ/219094", "url5": "kuchini.site:8080/infosat/df3093b6a3/279255" }
+
+]
