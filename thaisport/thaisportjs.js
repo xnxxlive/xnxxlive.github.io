c2c8c683c77a0ea7c0e006212f538f0d7a8abecf
xnxxlive <155718186+xnxxlive@users.noreply.github.com>
2024-02-12 03:20:12 +0630

    Update thaisportjs.js

--- thaisport/thaisportjs.js
+++ thaisport/thaisportjs.js
@@ -1,4 +1,4 @@
-const url = "./thaisport/thaisportm3u.js"
+const url = "https://xnxxlive.github.io/thaisport/thaisportm3u.js"
 fetch(url)
   .then((response) => {
   const urlData = response.json();
